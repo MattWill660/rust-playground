ed5020e590cb8ea859b0d24d0f0759b570c4efd0
Matt Willmering <matt.willmering@gmail.com>
2022-12-01 23:47:27 -0500

    Through chapter 1 of The Rust Book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+
+    for _i in 0..2 {
+        // underscore to signify unused variable
+        say(message.as_bytes(), width, &mut writer).unwrap();
+    }
 }
